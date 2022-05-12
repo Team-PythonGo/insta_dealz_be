a7dba4d3ba2fc2edeb6973208c9b86f1bc6effac
Wenhao Piao <wenhaopi@amazon.com>
2022-05-11 18:55:22 -0700

    Change product model fileds

--- instadealz/models.py
+++ instadealz/models.py
@@ -3,11 +3,8 @@ from django.db import models
 
 
 class Product(models.Model):
+    image = models.ImageField(default="")
     name = models.CharField(max_length=256)
-    owner = models.ForeignKey(
-        get_user_model(), on_delete=models.CASCADE, null=True, blank=True
-    )
-    description = models.TextField(default="", null=True, blank=True)
 
     def __str__(self):
         return self.name
