5dbacda2baf230fa3d04cde3e961a5621cdc5a17
Wenhao Piao <wenhaopi@amazon.com>
2022-05-11 21:01:55 -0700

    Build root api and /product api

--- instadealz/models.py
+++ instadealz/models.py
@@ -3,12 +3,13 @@ from django.db import models
 
 
 class Product(models.Model):
-    image = models.CharField(default="",max_length=256)
+    image = models.CharField(default="", max_length=256)
     name = models.CharField(max_length=256)
     description = models.TextField(default="")
     details = models.TextField(default="")
     price = models.IntegerField(default=0)
-
+    owner = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
